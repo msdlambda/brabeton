9bd6bcd666bb7d075949cb5159376846af933235
mohamed.msaad <msdlambda@gmail.com>
2022-05-22 19:23:14 +0200

    update for mail sender

---
+++ core.php
@@ -0,0 +1,25 @@
+<?php
+
+$miamail = "info@centroconcreto.com";
+
+$successo = $_POST['successo'];
+
+$nome = $_POST['nome'];
+
+$email = $_POST['email'];
+
+$telefono = $_POST['telefono'];
+
+$oggetto = $_POST['oggetto'];
+
+$msg = $_POST['messaggio'];
+
+$messaggio = "Nombre: " . $nome . "\nEmail: " . $email . "\nAsunto del email: " . $telefono . "\nTeléfono: " . $oggetto . "\nMensaje: " . $msg;
+
+$headers = "From: " . $miamail . "\r\n" . "Reply-To: " . $miamail;
+
+mail($miamail,"Mensaje del sitio",$messaggio,$headers);
+
+header("location: " . $successo);
+
+?>
